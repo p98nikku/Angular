b079074351cffcc8068e30694ca7d07deae60a0d
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-04-01 19:38:23 +0530

    lab3

---
+++ Angular3/src/app/tshirtdatabinding/tshirtdatabinding.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tshirtdatabinding',
+  templateUrl: './tshirtdatabinding.component.html',
+  styleUrls: ['./tshirtdatabinding.component.css']
+})
+export class TshirtdatabindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  title="Choose Tshirt";
+
+  //title = 'my-app';
+  h=300;
+  w=1000;
+  imgSource='../assets/front.jpg';
+
+  ChangeTshirtMouseEnter():void
+  {
+    this.imgSource='../assets/back.jpg';
+  }
+  ChangeTshirtMouseExit():void
+  {
+    this.imgSource='../assets/front.jpg';
+  }
+  setWidth():void{
+    this.h=100;
+    this.w=100;
+  }
+}
