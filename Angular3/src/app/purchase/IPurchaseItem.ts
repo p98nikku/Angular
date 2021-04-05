f1db3e8fbe7804b2fcc1b26c21db451b96998601
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-04-05 22:48:52 +0530

    c

--- Angular3/src/app/purchase/IPurchaseItem.ts
+++
@@ -1,9 +0,0 @@
-import { IProduct } from "../product/IProduct";
-
-export interface IPurchaseItem{
-    Pid:number,
-    Ptitle:string,
-    Pdate:string,
-    Pvendorname:string,
-    Items: IProduct[]
-}
