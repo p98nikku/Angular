5ea06a2776b6c1334b7ff2e3afd23efe2f821f42
Neetika Paliwal <neetikapaliwal19982016@gmail.com>
2021-04-20 19:47:17 +0530

    new commit

--- AngularProductCatalog/src/app/product/Iproduct.ts
+++
@@ -1,9 +0,0 @@
-export interface IProduct{
-    Id:number;
-    Name: string;
-    Manufacturer: String;
-    Description: string;
-    Category:string;
-    SellingPrice:number;
-    ShortCode:string;
-}
